b8d92b0275ad0c26f78c3c152faa6ee7867ba6ea
Carlos Gonzalez <carlosg2011@gmail.com>
2021-04-17 12:42:06 -0700

    updated basic-info component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
 import { BasicInfoComponent } from './pages/basic-info/basic-info.component';
 import { InDepthInfoComponent } from './pages/in-depth-info/in-depth-info.component';
 import { LandingComponent } from './pages/landing/landing.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'basicinfo', component: BasicInfoComponent},
   { path: 'indepthinfo', component: InDepthInfoComponent},
+  { path: 'about', component: AboutComponent},
   { path: '**', redirectTo: '' }
 ];
 
