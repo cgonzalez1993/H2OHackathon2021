75a933310bfa03a0aa6e0e413e7ff4fcc560f639
Carlos Gonzalez <carlosg2011@gmail.com>
2021-04-17 09:20:59 -0700

    added components for seperate pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LandingComponent } from './pages/landing/landing.component';
+import { BasicInfoComponent } from './pages/basic-info/basic-info.component';
+import { InDepthInfoComponent } from './pages/in-depth-info/in-depth-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    BasicInfoComponent,
+    InDepthInfoComponent
   ],
   imports: [
     BrowserModule,
